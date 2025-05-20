b2c0a74d9997ca2a0ddb01c97da5d5f06e75f211
Michael Ivy <ivym8@gmatc.matc.edu>
2025-05-20 14:03:39 -0500

    Completed Activity 7

---
+++ activity-7/js/scripts.js
@@ -0,0 +1,62 @@
+var tasks = [];
+
+var taskStatus = {
+    active: 'active',
+    completed: 'completed'
+};
+
+function Task(id, name, status) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+}
+
+function addTaskElement(task) {
+    var listEL = document.getElementById('active-list');
+    var taskEL = document.createElement('li');
+    var textEL = document.createTextNode(task.name);
+
+    taskEL.setAttribute('id', task.id);
+    taskEL.appendChild(textEL);
+    listEL.appendChild(taskEL);
+}
+
+function addTask(event) {
+    var inputEL = document.getElementById('input-task');
+    if (inputEL.value != '') {
+        var id = 'item-' + tasks.length;
+        var task = new Task(id, inputEL.value, taskStatus.active);
+        tasks.push(task);
+        addTaskElement(task);
+        inputEL.value = '';
+    }
+}
+
+function completeTask(event) {
+    var taskEL = event.target;
+    var id = taskEL.id;
+
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === id) {
+            tasks[i].status = taskStatus.completed;
+            break;
+        }
+    }
+
+    taskEL.remove();
+    document.getElementById('completed-list').appendChild(taskEL);
+}
+
+function clickButton(event) {
+    if (event.keyCode === 13) {
+        document.getElementById('add-task').click();
+    }
+}
+
+function init() {
+    document.getElementById('add-task').onclick = addTask;
+    document.getElementById('active-list').onclick = completeTask;
+    document.getElementById('input-task').onkeypress = clickButton;
+}
+
+init();
